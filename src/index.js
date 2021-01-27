b5dd338826c494835585110f5da9d0d9bbe2bd20
David Gatica <davgatik@gmail.com>
2021-01-27 01:07:26 -0600

    Instalacion y Configuracion  de Redux en React

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
 import App from './routes/App';
+import {Provider} from 'react-redux';
+import {createStore} from  'redux'; // Logica para tener el  store y compartirla a la aplicacion
 
 
 
-ReactDOM.render(<App/>,document.getElementById('app'));
+ReactDOM.render(
+	<Provider>
+		<App/>
+	</Provider>,
+	document.getElementById('app')
+);
 
